937d092a050bb59a04c06773cb63d21c00f65fca
fishboy <paul@2fishconsulting.com>
2024-04-08 12:54:29 +0100

    Update lora_mqtt_bridge.cpp

--- esphome/components/lora_mqtt_bridge/lora_mqtt_bridge.cpp
+++ esphome/components/lora_mqtt_bridge/lora_mqtt_bridge.cpp
@@ -64,7 +64,7 @@ namespace esphome
                 crc16_hash = crc16((const uint8_t*)dataline, sizeof(dataline));
 
                 ESP_LOGD(TAG, "*fishboy* received dataline: %s", dataline);
-                ESP_LOGD(TAG, "*fishboy* received dataline size: %s", sizeof(dataline));
+                ESP_LOGD(TAG, "*fishboy* received dataline size: %i", sizeof(dataline));
 
                 ESP_LOGD(TAG, "*fishboy* calculated crc hash: %i", crc16_hash);                
                 ESP_LOGD(TAG, "*fishboy* received crc hash: %s", crcinfo);
